1e2168f60ce5cfe0c6aed57c6bfde2d7ac8b19db
Camila Figueiredo <camila_fig@hotmail.com>
2024-06-02 18:56:45 -0300

    new navigation

--- src/router/navigation/navigation.jsx
+++ src/router/navigation/navigation.jsx
@@ -1,7 +1,32 @@
+import barril from '../../assets/barril.png'
+import pesquisar from '../../assets/search.png'
+import { CartIcon } from '../../components/cart-icon/cart-icon'
+import './navigation.scss'
+
 export function Navigation() {
     return (
-        <div className="navigation">
-            <h1>NAVIGATION</h1>
-        </div>
+        <>
+            <div className="navigation">
+                <div className="nav-logo">
+                    <div className="nav-logo">
+                        <img className="nav-logo-image" src={barril} alt="Imagem de barril" />
+                    </div>
+                    <div className="store-name">
+                        <p className="store-name-adega">Adega</p>
+                        <p className="store-name-online">Online</p>
+                    </div>
+                </div>
+                <div className='nav-search'>
+                    <input className='nav-search-text' type="search" placeholder='pesquisar um rótulo' />
+                    <img className='nav-icon-search' src={pesquisar} alt="Pesquisar" />
+                </div>
+                <div className="icon-container">
+                    <p className="nav-link">Favoritos</p>
+                    <p className="nav-link">Entrar</p>
+                </div>
+                <CartIcon />
+            </div>
+            <Outlet />
+        </>
     )
 }
