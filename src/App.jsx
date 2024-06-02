220e4e77b24b5cffa7b6737dee8780d7517f5634
Camila Figueiredo <camila_fig@hotmail.com>
2024-06-02 19:45:04 -0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.scss'
-import { Footer } from './components/footer/footer'
-import { PosterOverlay } from './components/poster-overlay/poster-overlay'
 import { Home } from './router/home/home'
 import { Navigation } from './router/navigation/navigation'
-import { SaleCellar } from './router/sale-cellar/sale-cellar'
 import { Shop } from './router/shop/shop'
+import './App.scss'
 
 export function App() {
-    return (
+  return (
     <>
-    <Routes>
-
-      <Route path='/' element={<Navigation/>}>
-        <Route index element={<Home/>}/>
-        <Route path='shop/*' element={<Shop/>}/>
-      </Route>
-
-
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home/>}/>
+          {/* <Route path='shop/*' element={<Shop/>}/> */}
+        </Route>
+      </Routes>
     </>
   )
 }
