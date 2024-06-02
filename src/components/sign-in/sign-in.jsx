7f354f5c291452a63980394d957bde9c38ff1f70
Camila Figueiredo <camila_fig@hotmail.com>
2024-06-02 18:53:59 -0300

    sign

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { Button } from "../button/button";
+import { FormInput } from "../form-input/form-input";
+import './sign-in.scss'
+
+const defaultFormFields = {
+    email: "",
+    senha: ""
+}
+
+export function SignIn() {
+
+    const [formFilds, setFormFilds] = useState(defaultFormFields)
+    const { email, senha } = formFilds
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormFilds({ ...formFilds, [name]: value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setFormFilds(defaultFormFields)
+    }
+
+    return (
+        <div>
+            <p className="form-title">Você já possui conta?</p>
+            <form className="form-container" onSubmit={handleSubmit}>
+                <FormInput
+                    label="E-mail"
+                    type="email"
+                    requeried
+                    name="email"
+                    placeholder="digite seu e-mail"
+                    autocomplete="off"
+                    onChange={handleChange}
+                    value={email}
+                />
+                <FormInput
+                    label="Senha"
+                    type="password"
+                    requeried
+                    name="senha"
+                    placeholder="digite sua senha"
+                    onChange={handleChange}
+                    value={senha}
+                />
+                <Button type="submit">
+                    Entrar
+                </Button>
+                <Button type="button" typeButton="google">
+                    Entrar com conta Google
+                </Button>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/sign-up/sign-up.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Button } from "../button/button";
+import { FormInput } from "../form-input/form-input";
+
+const defaultFormFields = {
+    nome: "",
+    email: "",
+    senha: "",
+    confirmeSenha: ""
+}
+
+export function SignUp() {
+
+    const [formFilds, setFormFilds] = useState(defaultFormFields)
+    const { nome, email, senha, confirmeSenha } = formFilds
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setFormFilds({ ...formFilds, [name]: value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (senha !== confirmeSenha) {
+            alert("As senhas digitadas não são iguais")
+            return
+        }
+        setFormFilds(defaultFormFields)
+    }
+
+    return (
+        <div>
+            <p className="form-title">Você não possui conta?</p>
+            <form className="form-container" onSubmit={handleSubmit}>
+                <FormInput
+                    label="Nome"
+                    type="text"
+                    requeried
+                    name="nome"
+                    placeholder="digite seu nome"
+                    autocomplete="off"
+                    onChange={handleChange}
+                    value={nome}
+                />
+                <FormInput
+                    label="E-mail"
+                    type="email"
+                    requeried
+                    name="email"
+                    placeholder="digite seu e-mail"
+                    autocomplete="off"
+                    onChange={handleChange}
+                    value={email}
+                />
+                <FormInput
+                    label="Senha"
+                    type="password"
+                    requeried
+                    name="senha"
+                    placeholder="digite sua senha"
+                    autocomplete="off"
+                    onChange={handleChange}
+                    value={senha}
+                />
+                <FormInput
+                    label="Confirme sua senha"
+                    type="password"
+                    requeried
+                    name="confirmeSenha"
+                    placeholder="digite sua senha novamente"
+                    autocomplete="off"
+                    onChange={handleChange}
+                    value={confirmeSenha}
+                />
+                <Button type="submit">
+                    Cadastrar
+                </Button>
+            </form>
+        </div>
+    )
+}
