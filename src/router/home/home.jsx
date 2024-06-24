0113a00684f88a1ec1f364d79892d813e2bffa47
Camila Figueiredo <camila_fig@hotmail.com>
2024-06-24 18:27:26 -0300

    código limpo

--- src/router/home/home.jsx
+++ src/router/home/home.jsx
@@ -1,14 +1,10 @@
-import { Categories } from "../../components/categories/categories"
 import { Footer } from "../../components/footer/footer"
 import { PosterOverlay } from "../../components/poster-overlay/poster-overlay"
 import { SaleCellar } from "../sale-cellar/sale-cellar"
 
-
-
 export function Home() {
 return (
     <>
-    {/* <Categories categories={categories} /> */}
     <PosterOverlay />
     <SaleCellar />
     <Footer />
