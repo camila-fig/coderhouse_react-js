31c9a4b8a675d933871a2d20bd470e741cfc4ece
Camila Figueiredo <camila_fig@hotmail.com>
2024-06-24 13:53:50 -0300

    organizando código

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -1,33 +1,22 @@
-
-
 import './icon.scss'
-import trash from '../../assets/trash.png'
+import trash from '../../assets/bin.png'
 import mais from '../../assets/mais.png'
-import menos from '../../assets/menos.png' 
+import menos from '../../assets/menos.png'
 
-export function IconTrash(){
-
-return(
-    <>
-    <img className='icon-trash' src={trash} alt="" />
-    </>
-)
-
-
+export function IconTrash() {
+    return (
+        <img className='icon-trash' src={trash} alt="Deletar produto" />
+    )
 }
 
-export function IconMais(){
-   return(
-    <>
-    <img className='icon-trash' src={mais} alt="" />
-    </>
-   ) 
+export function IconMais() {
+    return (
+        <img className='icon-mais-menos' src={mais} alt="Acrecentar quantidade" />
+    )
 }
 
-export function IconMenos(){
-    return(
-     <>
-     <img className='icon-trash' src={menos} alt="" />
-     </>
-    ) 
- }
+export function IconMenos() {
+    return (
+        <img className='icon-mais-menos' src={menos} alt="Reduzir quantidade" />
+    )
+}
