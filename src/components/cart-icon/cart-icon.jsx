b20d6655c98c4ec772c104bbfcafac9403621d98
Anderson <andersantfer@gmail.com>
2024-06-24 13:23:22 -0300

    ajuste css

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -5,16 +5,13 @@ import './cart-icon.scss'
 
 export function CartIcon() {
 
-
     const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
 
-
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen) 
-    console.log("valor no carrinho",cartCount)
-    
+    console.log("valor no carrinho",cartCount)    
 
     return (
-        <div className='cart-container' onClick={toggleIsCartOpen} disabled={cartCount === 0}>
+        <div className='cart-container' onClick={toggleIsCartOpen}  disabled={cartCount === 0}>
             {/* <p className='cart-text'>Carrinho</p> */}
             <div className="img-cart-count">
                 <img className='cart-icon' src={carrinho} alt="Carrinho de compras" />
