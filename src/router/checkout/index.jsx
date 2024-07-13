5f1d10409f20a794e3981d6a5d8fc45cb4859adf
Camila Figueiredo <camila_fig@hotmail.com>
2024-07-13 18:12:24 -0300

    otimizado

--- src/router/checkout/index.jsx
+++ src/router/checkout/index.jsx
@@ -2,52 +2,43 @@ import './style.scss'
 import { useContext } from 'react'
 import { CartContext } from '../../context/cartContext'
 import { CheckoutItem } from '../../components/checkout-item'
-
 import { Footer } from "../../components/footer/footer"
 import { UserContext } from '../../context/userContext'
 import { NavLink } from 'react-router-dom'
 
-
 export function Checkout() {
-
 
     const { cartItems, cartTotal } = useContext(CartContext)
     const { currentUser } = useContext(UserContext)
 
-    if (!currentUser) {
-        return (
-            <>
-                <div className='checkout-container'>
-                    <p className='checkout-title'>Os produtos selecionados foram os seguintes:</p>
-
-                    {
-                        cartItems.map((cartItem) => <CheckoutItem cartItem={cartItem} key={cartItem.id} />)
-                    }
-                    <div className='total-pay-container'>
-                        <p className='total-pay'>Valor total da compra: R$ {cartTotal},00</p>
-                        <NavLink to='/auth'>
-                            <div className='btn-logar'>Entre na sua conta para finalizar o pedido ou cadastre-se</div>
-                        </NavLink>
-                    </div>
+return (
+    <>
+{!currentUser &&
+             <div className='checkout-container'>
+                <p className='checkout-title'>Os produtos selecionados foram os seguintes:</p>
+                {
+                    cartItems.map((cartItem) => <CheckoutItem cartItem={cartItem} key={cartItem.id} />)
+                }
+                <div className='total-pay-container'>
+                    <p className='total-pay'>Valor total da compra: R$ {cartTotal},00</p>
+                    <NavLink to='/auth'>
+                        <div className='btn-logar'>Entre na sua conta para finalizar o pedido ou cadastre-se</div>
+                    </NavLink>
                 </div>
-                <Footer />
-            </>
-        )
-    } else {
-        return (
-            <>
-                <div className='checkout-container'>
-                    <p className='checkout-title'>Os produtos selecionados foram os seguintes:</p>
-                    {
-                        cartItems.map((cartItem) => <CheckoutItem cartItem={cartItem} key={cartItem.id} />)
-                    }
-                    <div className='total-pay-container'>
-                        <p className='total-pay'>Valor total da compra: R$ {cartTotal},00</p>
-                        <button className='btn-total-pay'>Finalizar pedido</button>
-                    </div>
+            </div>
+            }
+            {currentUser &&
+             <div className='checkout-container'>
+                <p className='checkout-title'>Os produtos selecionados foram os seguintes:</p>
+                {
+                    cartItems.map((cartItem) => <CheckoutItem cartItem={cartItem} key={cartItem.id} />)
+                }
+                <div className='total-pay-container'>
+                    <p className='total-pay'>Valor total da compra: R$ {cartTotal},00</p>
+                    <button className='btn-total-pay'>Finalizar pedido</button>
                 </div>
-                <Footer />
-            </>
-        )
-    }
+            </div>}
+            <Footer />
+        </>
+    )
 }
