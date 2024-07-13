85c5870aaa2336d7c33c345158fbcf0b1cce3bc8
Camila Figueiredo <camila_fig@hotmail.com>
2024-07-13 16:32:19 -0300

    organização

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,11 +1,11 @@
+import "./cart-dropdown.scss"
 import React from "react";
 import { Button } from "../button/button"
 import { CartItem } from "../cart-item/cart-item"
 import { useContext } from "react"
 import { CartContext } from "../../context/cartContext"
 import { useNavigate } from 'react-router-dom'
-import emptCart from '../../assets/emptCart.png'
-import "./cart-dropdown.scss"
+import { EmptyCart } from "../empty-cart/empty-cart";
 
 export function CartDropdown() {
 
@@ -15,39 +15,32 @@ export function CartDropdown() {
         navigate('/checkout')
     }
 
-let onOffCheck = ''
-let onOffCart = ''
+    let onOffCheck = ''
+    let onOffCart = ''
 
-if(cartItems.length == 0){
-onOffCheck = 'none'
-onOffCart = 'block'
-}else{
-    onOffCheck = 'block'
-    onOffCart = 'none'
-}
+    if (cartItems.length == 0) {
+        onOffCheck = 'none'
+        onOffCart = 'block'
+    } else {
+        onOffCheck = 'block'
+        onOffCart = 'none'
+    }
 
+    const divStyle = {
+        display: onOffCheck,
+    };
+    const divCart = {
+        display: onOffCart
+    }
 
-const divStyle = {
-    display: onOffCheck,
-};
- const divCart = {
-    display: onOffCart
- }
-
-    
-
-console.log('itens adicionados',cartItems.length)
+    // console.log('itens adicionados',cartItems.length)
 
     return (
         <div className="cart-dropdown-container">
-
-            <div className="img-empt-cart" style={divCart}>
-                <img src={emptCart} alt="Empt Cart" />
-                <br />
-                <h3>Nada aqui!!!</h3>
+            <div className="img-empty-cart" style={divCart}>
+                <EmptyCart />
             </div>
-
-            <div className="cart-items"  style={divStyle}>
+            <div className="cart-items" style={divStyle}>
                 {
                     cartItems.map(item => (
                         <CartItem cartItem={item} key={item.id} />
