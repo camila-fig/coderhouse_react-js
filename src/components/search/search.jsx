048e9072840f7d2c64299cff1b7088b958434313
Camila Figueiredo <camila_fig@hotmail.com>
2024-07-14 16:41:56 -0300

    search

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -7,7 +7,7 @@ import pesquisar from '../../assets/search.png'
 export function Search() {
 
     //Une as categorias e cria um array apenas com os produtos do arquivo shop-data.js
-    const products = WINE_LIST[0].items.concat(WINE_LIST[1].items).concat(WINE_LIST[2].items).concat(WINE_LIST[3].items)
+    const products = WINE_LIST[0].items.concat(WINE_LIST[1].items).concat(WINE_LIST[2].items).concat(WINE_LIST[3].items).concat(WINE_LIST[4].items)
     // console.log("Produtos", products)
 
     //Mecanismo de busca
@@ -15,12 +15,7 @@ export function Search() {
     const lowerSearch = search.toLowerCase()
     const productsFiltered = products
         .filter((product) => product.name.toLowerCase().includes(lowerSearch))
-    console.log("Filtrado", productsFiltered)
-    
-    const searchResults = productsFiltered.map((product) => (
-        <li key={product.id}>
-            <span>{product.name}</span>
-        </li>))
+    // console.log("Filtrado", productsFiltered)
 
     return (
         <>
@@ -36,9 +31,15 @@ export function Search() {
                     <img className='search-icon' src={pesquisar} alt="Pesquisar" />
                 </div>
                 <div className="search-results">
-                    {/* <Link to={`/product-detail/${productId}`}> */}
-                    <ul style={{ display: !search ? 'none' : 'grid' }}>{searchResults}</ul>
-                    {/* </Link> */}
+                    <ul style={{ display: !search ? 'none' : 'grid' }}>
+                        {productsFiltered.map((product) => (
+                            <li key={product.id}>
+                                <Link to={`/product-detail/${product.id}`}>
+                                    <span>{product.name}</span>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </>
