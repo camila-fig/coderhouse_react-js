837d08f380ed83a6848bb6b6dfd1fb589255bb1c
Camila Figueiredo <camila_fig@hotmail.com>
2024-07-14 01:44:09 -0300

    search

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,21 +1,22 @@
 import './search.scss'
 import { useState } from "react";
 import { WINE_LIST } from "../../../wine-list.js"
+import { Link } from "react-router-dom";
 import pesquisar from '../../assets/search.png'
 
 export function Search() {
 
     //Une as categorias e cria um array apenas com os produtos do arquivo shop-data.js
-    const products = WINE_LIST[0].items.concat(WINE_LIST[1].items)
-    // console.log(products)
+    const products = WINE_LIST[0].items.concat(WINE_LIST[1].items).concat(WINE_LIST[2].items).concat(WINE_LIST[3].items)
+    // console.log("Produtos", products)
 
     //Mecanismo de busca
     const [search, setSearch] = useState('')
     const lowerSearch = search.toLowerCase()
     const productsFiltered = products
         .filter((product) => product.name.toLowerCase().includes(lowerSearch))
-    // console.log(productsFiltered)
-
+    console.log("Filtrado", productsFiltered)
+    
     const searchResults = productsFiltered.map((product) => (
         <li key={product.id}>
             <span>{product.name}</span>
@@ -35,7 +36,9 @@ export function Search() {
                     <img className='search-icon' src={pesquisar} alt="Pesquisar" />
                 </div>
                 <div className="search-results">
+                    {/* <Link to={`/product-detail/${productId}`}> */}
                     <ul style={{ display: !search ? 'none' : 'grid' }}>{searchResults}</ul>
+                    {/* </Link> */}
                 </div>
             </div>
         </>
